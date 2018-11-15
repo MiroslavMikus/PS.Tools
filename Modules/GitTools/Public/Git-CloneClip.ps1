d77a5a9abd910c252a39ff5fac853910a268b5e9
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-11-15 20:50:38 +0100

    Add Git-CloneClip

--- Modules/GitTools/Public/Git-CloneClip.ps1
+++ Modules/GitTools/Public/Git-CloneClip.ps1
@@ -1,42 +1,32 @@
 # Miroslav Mikus 2018/04/26
 # 
-# Clone git url form clipboard to $root
+# Clone git url form clipboard to repository directory
 # 
 
 function Git-CloneClip {
     param (
-        [string]$root
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()] 
+        [string]$RepositoryDirectory,
+        [switch]$UseSleep = $false
     )
+
+    $logPath = Get-LogPaht "Git-AutoPush";
+
+    $clip = get-clipboard;
     
+    Write-log "Clip: $clip" -Path $logPath 
+    
+    Set-Location $RepositoryDirectory;
+    
+    Write-log "Run: git clone $clip" -Path $logPath 
+    
+    git clone $clip
+
+    if ($UseSleep){
+
+        Write-Host "Done - Script will end in 4 sec";
+        
+        Start-Sleep -s 4
+    }
 }
-
-#region import logger
-$scriptPath = (split-path $MyInvocation.MyCommand.Path);
-$scriptParent = (get-item $scriptPath).Parent.FullName
-$loggerPath = $scriptParent + "\Shared\Logger.ps1";
-$logPath = "$scriptPath\Log\$($MyInvocation.MyCommand.Name).log";
-. $loggerPath;
-#endregion
-
-if($root -eq ""){
-    
-    Write-log "Root parameter is missing" -Path $logPath -Level Error
-    
-    Read-Host -Prompt "Press Enter to exit"
-    
-    return;
-}
-
-$clip = get-clipboard;
-
-Write-log "Clip: $clip" -Path $logPath 
-
-Set-Location $root;
-
-Write-log "Run: git clone $clip" -Path $logPath 
-
-git clone $clip
-
-Write-Host "Done - Script will end in 4 sec";
-
-Start-Sleep -s 4
