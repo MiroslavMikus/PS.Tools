1734c3db1b2872d2870fc90131cb3a86b851ad1b
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-11-16 21:04:23 +0100

    move Get-logPath method to logger module

--- Modules/SimpleLogger/SimpleLogger.psm1
+++ Modules/SimpleLogger/SimpleLogger.psm1
@@ -96,3 +96,24 @@ function Write-Log
     { 
     } 
 }
+
+function Get-LogPaht {
+    param (
+        [Parameter(Mandatory=$false)] 
+        [ValidateNotNullOrEmpty()] 
+        [string]$ScriptName
+    )
+
+    $path = "$([Environment]::GetFolderPath('CommonApplicationData'))\PowershellLogs"
+
+    If(!(test-path $path))
+    {
+        New-Item -ItemType Directory -Force -Path $path
+    }
+    
+    return "$path\$ScriptName.log"
+}
+
+function Open-DefaultLogPath{
+    explorer.exe "$([Environment]::GetFolderPath('CommonApplicationData'))\PowershellLogs"
+}
