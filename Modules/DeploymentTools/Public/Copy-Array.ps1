5843b5e64cc9b5cea72926ea98f647a987eed92d
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-11-16 21:04:40 +0100

    update copy-array function

--- Modules/DeploymentTools/Public/Copy-Array.ps1
+++ Modules/DeploymentTools/Public/Copy-Array.ps1
@@ -1,20 +1,20 @@
 function Copy-Array {
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string[]]$Sources,
-        [Parameter(Mandatory = $true, Position = 1)]
+        [Parameter(Mandatory = $true)]
         [string]$Destination,
         [Parameter(Position = 2)]
         [switch]$UseIgnoreFile = $false,
         [Parameter(Position = 2)]
-        [string[]]$IgnoreFilter = @(),
-        [switch]$WhatIf
+        [string[]]$IgnoreFilter = @()
     )
     
     try {
         # check if path exist -> if not create one
         if (!(test-path $Destination)) {
-            Write-Host "Creating destination folder: $Destination";
+
+            Write-Log "Creating destination folder: $Destination";
             
             New-Item -ItemType Directory -Path $Destination;
         }
@@ -23,27 +23,20 @@ function Copy-Array {
         Read-Host "Error while creating directory: " + $Destination +" ; press enter to continue script"
     }
 
-    try {
-        switch ($PsCmdlet.ParameterSetName) {
-            "UseIgnoreFile" {
-                $Sources | ForEach-Object 
-                {
-                    Copy-Item -Path $_ -Destination $Destination -Exclude (Get-IgnoreFile $_) -Force 
-                }
-            }
-            "IgnoreFilter" {
-                $Sources | ForEach-Object 
-                { 
-                    Copy-Item -Path $_ -Destination $Destination -Exclude $IgnoreFilter -Force 
-                }
-            }
+    if ($UseIgnoreFile) {
+
+        $Sources | ForEach-Object `
+        {
+            # load new ignore file for each element in source array
+            Copy-Item -Path $_ -Destination $Destination -Exclude (Get-IgnoreFile $_) -Force 
         }
     }
-    catch {
-        Write-Host "Error while copying files";
-        Write-Host "Sources: " + $Sources;
-        Write-Host "Destination: " + $Destination;
-        Read-Host "Press enter to continue script";
+    else {
+
+        $Sources | ForEach-Object `
+        {
+            Copy-Item -Path $_ -Destination $Destination -Exclude $IgnoreFilter -Force 
+        }
     }
 }
 
--- Modules/DeploymentTools/Public/Get-IgnoreFile.ps1
+++ Modules/DeploymentTools/Public/Get-IgnoreFile.ps1
@@ -3,18 +3,19 @@ function Get-IgnoreFile {
         $Path
     )
     
-    $ignoreFile = Join-Path $Path ".fileignore";
+    $ignoreFile = Join-Path ([io.Path]::GetDirectoryName($Path)) ".fileignore";
     
-    if(Test-Path $ignoreFile){
+    if (Test-Path $ignoreFile) {
 
-        Write-Host "Ignore file was found"
+        Write-Host "Ignore file was found: $ignoreFile"
 
         $ignoreText = Get-Content $ignoreFile;
 
         return $ignoreText
 
-    } else {
-        Write-Host "Ignore file was not found"
+    }
+    else {
+        Write-Host "Ignore file was not found: $ignoreFile"
         
         return @();
     }
