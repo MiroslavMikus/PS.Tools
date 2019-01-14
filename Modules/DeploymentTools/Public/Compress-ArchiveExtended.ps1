9a8bd9c623cc059f141617bb8b8e938a8b758a81
Miroslav Mikus <miroslav.mikus@outlook.com>
2019-01-14 22:15:27 +0100

    DeploymentTools: add InvertFileIgnore

--- Modules/DeploymentTools/Public/Compress-ArchiveExtended.ps1
+++ Modules/DeploymentTools/Public/Compress-ArchiveExtended.ps1
@@ -1,10 +1,32 @@
 function Compress-ArchiveExtended {
     param (
         [string]$Path,
-        [string]$DestinationPath
+		[string]$DestinationPath,
+		[switch]$InvertFileIgnore
     )
 
-    $ignoreText = Get-IgnoreFile $Path
+	$ignoreText = Get-IgnoreFile $Path
+	
+	Write-Host "Ignore content: " + $ignoreText
+	
+	if ($InvertFileIgnore) {
 
-    Get-ChildItem $Path -Exclude $ignoreText | Compress-Archive -DestinationPath $DestinationPath -CompressionLevel Fastest
+		Write-Host "Reverting ignore file"
+
+		Get-ChildItem $Path |
+			Where-Object {
+				foreach ($filter in $ignoreText) {
+					if ($_ -match $filter) {
+						return $true;
+					}
+				}
+				return $false;
+			} |
+				Compress-Archive -DestinationPath $DestinationPath -CompressionLevel Fastest
+	}
+	else {
+		Get-ChildItem $Path -Exclude $ignoreText | Compress-Archive -DestinationPath $DestinationPath -CompressionLevel Fastest
+	}
 }
+
+Compress-ArchiveExtended -Path C:\temp\ps1 -DestinationPath C:\temp\ps2\target.zip -InvertFileIgnore
