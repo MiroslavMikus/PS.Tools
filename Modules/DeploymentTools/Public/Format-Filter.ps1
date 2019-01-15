18e2543fc3549e9c8a09e2a7cddd13e91f9494d1
Miroslav Mikus <miroslav.mikus@outlook.com>
2019-01-15 19:11:09 +0100

    DeploymentTools: Add Format-Filter

---
+++ Modules/DeploymentTools/Public/Format-Filter.ps1
@@ -0,0 +1,17 @@
+function Format-Filter{
+    param (
+		[ValidateNotNullOrEmpty()]
+        [string[]]$FilterContent
+    )
+    $result = @();
+
+    foreach ($filter in $FilterContent) {
+        if ($filter.StartsWith('*')) {
+                $result += "{0}{1}" -f '.', $filter 
+            }
+            else {
+                $result = $filter
+        }
+    }
+    return $result;
+}
