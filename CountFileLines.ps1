b06e8e9b811edfbd0c78b7e0a1281799b8936394
Miroslav Mikus <miroslav.mikus@outlook.com>
2021-05-25 11:44:00 +0200

    Add Count files

---
+++ CountFileLines.ps1
@@ -0,0 +1,22 @@
+﻿$sources = "<rootPath>","<rootPaht>"
+
+$lines = @()
+
+
+$result = $sources | foreach {
+    $files = Get-ChildItem -Path $_ -Recurse -Filter $filter -Exclude "*i.cs","*g.cs";
+    $count = 0;
+    Foreach ( $file in $files)
+    {
+        $count += (Get-Content $file.FullName | Measure-Object –Line).Lines
+    }
+    $lines += "$_;$count;$filter"
+}
+
+Add-Content C:\S\AllLines.csv $lines
+
+
+$filter = "*.cs"
+$filter = "*.xaml"
+$filter = "*.TcPOU"
+$filter = "*.ps1"
